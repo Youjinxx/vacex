4aabfb35ec73bbfba5ebabcf800047bc73672b89
Youjinxx <flourishxx7@gmail.com>
2023-02-14 22:40:33 +0900

    post 화면과 리스트

--- excercise/urls.py
+++ excercise/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("main/", include('main.urls')), #main의 url을 포함한다. main에 url.py 파일을 만든다.
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
+def index(request):
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'main/index.html',
+        {
+            'posts':posts,
+        }
+    )
 # Create your views here.
